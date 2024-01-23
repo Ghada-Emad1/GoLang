22539ef254b86fe187fc6b93b90663e56b6abea6
ghada <ghadaemad011@gmail.com>
2024-01-23 17:01:22 +0200

    finished primitive types

--- primitive.go
+++ primitive.go
@@ -1,5 +1,6 @@
 package main
-import(
+
+import (
 	"fmt"
 )
 func main(){
@@ -28,5 +29,64 @@ func main(){
 	var num3 int=3
 	var num4 int16=1234
 	fmt.Println(num3+int(num4))
+	a:=10;
+	b:=3;
+	fmt.Println(a&b);
+	fmt.Println(a|b);
+	fmt.Println(a^b);
+	fmt.Println(a&^b);
+
+	//bit shift
+	a1:=8;
+	//when we shift to left we multiply
+	fmt.Println(a1<<3); //shift to left = 2^3 * 2^3 =2^6
+	fmt.Println(a1<<2); // = 2^3 *2^2=2^5
+	fmt.Println(a1<<1); // =2^3* 2^1=2^4
+
+	// when we shift to right we divivde
+	fmt.Println(a1>>3); // shift right = 2^3/2^3=2^0 =1
+	fmt.Println(a1>>2); // =2^3/2^2 = 2^1 =2
+	fmt.Println(a1>>1) // =2^3 /2^1 = 2^2 =4
+
+	//Floating number : float is 32 or 64 bit version
+	n1:=0.4
+	n1=13.1e72
+	n1=2.1E14
+	fmt.Printf("%v,%T\n",n1,n1);
+
+	v:=10.2
+	m1:=3.7
+	fmt.Println(v+m1)
+	fmt.Println(v-m1)
+	//fmt.Println(v%m1) -->error we can't take remainder of two floating numbers
+
+	//Complex Numbers : is 64 or 128 bit version
+	var n2 complex64=1+2i 
+	fmt.Printf("%v,%T\n",n2,n2);
+	//To separate real and imaginary numbers we can use built-in function (real-imag)
+	fmt.Printf("%v,%T\n",real(n2),real(n2));
+	fmt.Printf("%v,%T\n",imag(n2),imag(n2))
+
+	//another way to decalre complex number
+	var complx complex128=complex(5,12);
+	fmt.Printf("%v,%T\n",complx,complx);
+
+	//String  : represent Utf8 -it is immutable
+	s:="This is a string variable"
 	
+	fmt.Printf("%v,%T\n",s,s)
+	fmt.Printf("%v,%T\n",s[1],s[1]) //string in go alises for bytes
+	fmt.Printf("%v,%T\n",string(s[1]),s[1]) //type - uint8
+	//concating
+	s2:=" another string"
+	fmt.Println(s+s2);
+   
+	b1:=[]byte(s) //convert string into a collection of byte
+	fmt.Printf("%v,%T\n",b1,b1);
+	
+
+	//Rune : represent  utf32 character- Rune is an integer 32
+	r:='r'
+	fmt.Printf("%v,%T\n",r,r)
+	fmt.Printf("%v,%T\n",string(r),r)
 }
